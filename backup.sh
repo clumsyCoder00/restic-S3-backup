b13cf16169718997c3ca36755a16dd96102a6418
clumsyCoder00 <nuthanael.watkins@gmail.com>
2022-11-20 22:12:06 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 
-# configure auto-run with sudo crontab -e
-# 0 2 * * * '/path/to/folder/backup.sh'
-
-# ---- Restic Info ----
-# TODO
-# https://forum.restic.net/t/huge-amount-of-data-read-from-s3-backend/2321
-
-# x figure out what data can be moved to deep archive, looks like everything with the exception of the data dir
-#    https://forum.restic.net/t/s3-deep-glacier-any-experience-with-costs/3285/4
-#    https://forum.restic.net/t/s3-deep-glacier-any-experience-with-costs/3285/19
-
-
-# restic commands
-# use source with sudo
-# sudo su
-# source '/home/nuthanael/Documents/backup/restic/.Lo.restic.env'
-
-# restic init - create a new respository in bucket
-# restic check - verify data is properly stored in repository
-# restic snapshots - list snapshots in existing repository
-# restic restore 'snapshot_name' --target 'restore_path' - restore a partiuclar snapshot
-# restic forget 'snapshot_name' --prune - delete snapshots, prune aws storage
-# restic forget --keep-tag anything --prune - delete snapshots, prune aws storage
-# restic backup --files-from restic.files --tag seed (--dry-run) (--verbose=2)
-# restic prune - remove unreferenced packs - started backing up data I didn't want, completed subsequent backup. now unwanted data is no longer referenced by any current, complete snapshot
-
-# cache
-# https://restic.readthedocs.io/en/latest/manual_rest.html#caching
-# set cache directory with XDG_CACHE_HOME
-# this needs to be backed up in order to access snapshot info without access to Glacier archive
-
-# forget
-# forget --keep-within-daily 7d --keep-within-weekly 1m --keep-within-monthly 1y --keep-within-yearly 75y --prune
-
-# set S3 Storage Class
-# -o s3.storage-class=REDUCED_REDUNDANCY
-# STANDARD, STANDARD_IA, ONEZONE_IA, INTELLIGENT_TIERING, REDUCED_REDUNDANCY
 # ---- Restic Info ----
 
 SCRIPT=$(readlink -f "$0")
